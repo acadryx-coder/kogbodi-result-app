dbb19f570388bb53e139bbcf104df01f3c6fd45d
acadryx-coder <acadryx.os@gmail.com>
2025-12-28 00:16:48 +0100

    Revert middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { createMiddlewareClient } from '@supabase/ssr'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
-export async function middleware(req: any) {
+export async function middleware(req) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
   await supabase.auth.getSession()
